0fc7063b5ac3dde0956994030b5c3af611bfefb8
Bruno Vecchi <vecchi.b@gmail.com>
2013-10-28 23:57:20 -0300

    Replace HTTP verb for modifying karma from GET to POST
    This makes it more RESTful by capturing the action of modifying a
    resource. The Location attribute in the header points to the modified
    resource.
    
    Had to take special care that the "see other" URI was well encoded to
    deal with names with spaces, slashes and other disallowed characters.

--- src/main/java/me/karma/core/Karma.java
+++ src/main/java/me/karma/core/Karma.java
@@ -2,14 +2,15 @@ package me.karma.core;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * A representation class of the karma value of something
  */
 public class Karma {
-    private final int value;
-
-    @NotEmpty
+    @NotNull
     private final String name;
+    private final int value;
 
     public Karma(String name, int value) {
         this.name = name;
--- src/main/java/me/karma/paths/Bump.java
+++ src/main/java/me/karma/paths/Bump.java
@@ -1,13 +1,17 @@
 package me.karma.paths;
 
 import com.yammer.metrics.annotation.Timed;
-import me.karma.core.Karma;
 import me.karma.db.KarmaDAO;
 
-import javax.ws.rs.*;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import static com.google.common.base.Preconditions.checkState;
+import javax.ws.rs.core.Response;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
 
 @Path("/{name}/bump")
 @Produces(MediaType.APPLICATION_JSON)
@@ -18,17 +22,14 @@ public class Bump {
         this.karmaStore = karma;
     }
 
-    @GET
+    @POST
     @Timed
-    public Karma bump(@PathParam("name") String name) {
+    public Response bump(@PathParam("name") String name) throws UnsupportedEncodingException {
         boolean success = karmaStore.bump(name) == 1;
 
         if (!success) karmaStore.insert(name, 1);
 
-        Integer value = karmaStore.get(name);
-
-        checkState(value != null, "Karma of [%s] cannot be null after bumping", name);
-
-        return new Karma(name, value);
+        String encodedName = URLEncoder.encode(name, "UTF-8").replaceAll("\\+", "%20");
+        return Response.seeOther(URI.create(encodedName)).build();
     }
 }
